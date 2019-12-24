ebea774e4537d446d7393775c8bd6d4f8d129830
jiaguangfeng <jiaguangfeng@eyou.net>
2019-12-24 16:59:35 +0800

    Initial commit

---
+++ src/com/qx/day01/day01.java
@@ -0,0 +1,30 @@
+package com.qx.day01;
+
+/**
+ * 简单的声明变量
+ */
+public class day01 {
+
+    public static void main(String[] args) {
+        //字符串
+        String name = "qx";
+        //整形
+        int age = 18;
+        //浮点型
+        float height = 1.8f;
+        //双精度
+        double mark = 99.8;
+        //bool
+        boolean flag = false;
+        //char
+        char a = 'a';
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(mark);
+        System.out.println(flag);
+        System.out.println(a);
+    }
+
+
+}
---
+++ src/com/qx/day01/day01.kt
@@ -0,0 +1,23 @@
+package com.qx.day01
+
+fun main() {
+    var name = "qx"
+    var age = 18
+    var height = 180.3
+    var mark = 18.33
+    var flag = false
+    var a = 'a'
+
+//    val name = "qx"
+//    val age = 18
+//    val height = 180.3
+//    val mark = 18.33
+//    val flag = false
+//    val a = 'a'
+    println(name)
+    println(age)
+    println(height)
+    println(mark)
+    println(flag)
+    println(a)
+}
