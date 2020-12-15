0406f46a5d97705093a7e25146c758a6c80de8f7
jiaguangf <jiagaungfeng>
2020-12-15 21:46:36 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/com/qx/day15/main.kt

--- src/com/qx/day15/main.kt
+++ src/com/qx/day15/main.kt
@@ -1,16 +1,38 @@
 package com.qx.day15
 
+
+fun main() {
+    val test:String=""
+
+    System.out.println(len(test))
+
+    val result = with(StringBuffer()){
+        for (tes in list){
+            append(tes)
+        }
+        append("aaaaa").toString()
+
+    }
+    System.out.println(result)
+
+}
+fun len(test:String):Int{
+    return test?.length
+}
+
+
+val  list = listOf<String>("aaaa","bbbb","ccc")
+
+
 fun main(args: Array<String>) {
+    val result = "testLet".let {
+        println(it.length)
+        1000
+    }
 
-    val text: String? = null;
-    val length = text?.length
-    System.out.println(length)
-
-    val text1 = """
-        |First Line
-        |Second Line
-        |Third Line
-            """.trimMargin()
-    System.out.println(text1)
-
+    val result1 = run {
+//        println("my name is 11, I am  11 years old, my phone number is 11")
+        1000
+    }
+    println(result1)
 }
